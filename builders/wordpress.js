801479d800d5fbc467d5067d1ed302b4290c0f9c
Alec Reynolds <alec+git@lando.dev>
2024-03-04 16:57:39 -0800

    Make sure we add dbConfig.

--- builders/wordpress.js
+++ builders/wordpress.js
@@ -167,11 +167,12 @@ const getConfigDefaults = options => {
   // Get the viaconf
   if (_.startsWith(options.via, 'nginx')) options.defaultFiles.vhosts = 'default.conf.tpl';
 
-  // Get the default db conf
-  const dbConfig = _.get(options, 'database', 'mysql');
+  // attempt to discover the database that is actually being used
+  // @NOTE: this will look to see if database is overridden
+  const dbConfig = getDatabaseType(options);
   const database = _.first(dbConfig.split(':'));
   const version = _.last(dbConfig.split(':')).substring(0, 2);
-  if (database === 'mysql' || database === 'mariadb') {
+  if (database === 'laravel-mysql' || database === 'mysql' || database === 'mariadb') {
     if (version === '8.') {
       options.defaultFiles.database = 'mysql8.cnf';
     } else {
